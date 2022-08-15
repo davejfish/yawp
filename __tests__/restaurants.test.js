8178dea47c0d8b9c050c8c7b63fd818f2a7f5662
Dave Fisher <dave.j.fish@gmail.com>
2022-08-15 16:06:41 -0700

    controller route for get all restaurants added

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -3,14 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const mockUser = {
-  username: 'fake', 
-  password: '123456',
-  email: 'test@example.com'
-};
-
-const agent = request.agent(app);
-
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);

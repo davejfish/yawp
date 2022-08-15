0940db77aa41b9f0d730fbce9b0bf466893cf076
Dave Fisher <dave.j.fish@gmail.com>
2022-08-15 10:57:23 -0700

    test written for creating new user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,16 +1,33 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  username: 'fake', 
+  password: '123456',
+  email: 'test@example.com'
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+  
   afterAll(() => {
     pool.end();
   });
+
+  it('#POST /user should create a new user', async () => {
+    const response = await request(app).post('/api/v1/user').send(mockUser);
+    const { username, email } = mockUser;
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      username,
+      email,
+    });
+  });
+
 });

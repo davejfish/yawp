f166ede1529216686fe8200bbcbdc7072ba75384
Dave Fisher <dave.j.fish@gmail.com>
2022-08-15 16:00:15 -0700

    updated sql tables for restaurants and reviews

---
+++ __tests__/restaurants.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  username: 'fake', 
+  password: '123456',
+  email: 'test@example.com'
+};
+
+const agent = request.agent(app);
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+
+  
+  
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
+DROP TABLE IF EXISTS rest_reviews;
+DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -10,9 +12,41 @@ CREATE TABLE users (
   email TEXT UNIQUE
 );
 
+CREATE TABLE restaurants (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE rest_reviews (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  rest_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  review varchar(255) NOT NULL,
+  FOREIGN KEY (rest_id) REFERENCES restaurants(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO restaurants
+(name)
+VALUES
+('Imagine'),
+('CoCoIchibanya'),
+('have a good slice');
+
 INSERT INTO users
 (username, password_hash, email)
 VALUES
 ('yawper', '$2b$10$GOi9xgKDsVwO8JXRr6jBlOkOm4DaZ3IKopWy6DBJ.BuAX79H1f6Yy', 'yawper@yawp.com'),
 ('critic', 'fakehash', 'critic@yawp.com'),
 ('kev', 'fakehash', 'stuff@yawp.com');
+
+INSERT INTO rest_reviews
+(rest_id, user_id, review)
+VALUES
+('1', '1', 'best miso ramen of all time'),
+('1', '1', '15/10 best ever'),
+('1', '3', 'karaage so juicy'),
+('2', '2', 'COCOICHI LETS GO'),
+('2', '1', 'KARAAGE CURRY!!!!'),
+('2', '3', 'veggie curry changed my life'),
+('3', '1', 'new york style pizza in Japan. 8/10.');
